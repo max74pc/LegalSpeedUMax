a2f00dbfb27ef3b04c9542ed24a8941046782c3a
Massimiliano Scriboni <massimiliano.scriboni@gmail.com>
2019-07-17 13:53:19 +0200

    Modificata finestra login inserendo usercontrol windows form

--- LSView/WPF/LSMainProgramWPF/LoginWindow.xaml.vb
+++ LSView/WPF/LSMainProgramWPF/LoginWindow.xaml.vb
@@ -4,6 +4,9 @@ Imports LSDataModel.LSDataModelNS
 Imports System.Windows.Forms
 
 Public Class LoginWindow
+    Public WinFormTxtUsername As New LSUltraCasellaTesto.LSUltraCasellaTesto
+    Public WinFormPWD As New LSUltraPassword.LSUltraPassword
+
     Private Sub BtnOK_Click(sender As Object, e As RoutedEventArgs) Handles BtnOK.Click
         Dim mainWindow As New MainWindow
         Dim listaParametri As New Dictionary(Of String, String)
@@ -11,23 +14,23 @@ Public Class LoginWindow
 
         Try
             'Codifico la password
-            If PwdBox.Password.ToString.Trim <> "" Then
-                sPasswordCriptata = ClsComune.CodificaPwd(PwdBox.Password.ToString.Trim)
+            If WinFormPWD.ValorePassword.Trim <> "" Then
+                sPasswordCriptata = ClsComune.CodificaPwd(WinFormPWD.ValorePassword.Trim)
             End If
 
             'Creo la lista dei fitri di ricerca
             If sPasswordCriptata <> "" Then
                 listaParametri.Add("PasswordCriptata", sPasswordCriptata)
             End If
-            If Trim(TxtEdUsername.Text.Trim <> "") Then
-                listaParametri.Add("NomeUtente", TxtEdUsername.Text.Trim)
+            If Trim(WinFormTxtUsername.ValoreCasellaTesto.Trim <> "") Then
+                listaParametri.Add("NomeUtente", WinFormTxtUsername.ValoreCasellaTesto.Trim)
             End If
 
             'Verifica Username e Password
             Dim listaUtenti As List(Of Utente) = Operazioni.CaricaDatiFiltrati(Of Utente)(listaParametri)
 
             'If listaUtenti.Count = 1 Then
-            ClsComune.GlbStrUserNameLS = TxtEdUsername.Text.Trim
+            ClsComune.GlbStrUserNameLS = WinFormTxtUsername.ValoreCasellaTesto.Trim
             Me.Close()
             mainWindow.Show()
             'Else
@@ -47,10 +50,14 @@ Public Class LoginWindow
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        Dim host As New Integration.WindowsFormsHost()
-        Dim WinFormPWD As New LSUltraPassword.LSUltraPassword
-        WinFormPWD.Visible = True
-        host.Child = WinFormPWD
-        MainGrid.Children.Add(host)
+        Dim host1 As New Integration.WindowsFormsHost()
+        Dim host2 As New Integration.WindowsFormsHost()
+        WinFormTxtUsername.TestoEtichetta = "Username:"
+        WinFormTxtUsername.LarghezzaEtichetta = 80
+        WinFormTxtUsername.LarghezzaCasellaTesto = 255
+        host1.Child = WinFormTxtUsername
+        StackPanel1.Children.Add(host1)
+        host2.Child = WinFormPWD
+        StackPanel2.Children.Add(host2)
     End Sub
 End Class
